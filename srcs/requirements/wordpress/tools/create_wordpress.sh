f8545b34d4738ca86e50088f1926c47d055b27eb
Lea Lescure <lea.lescure@hotmail.fr>
2021-08-02 12:18:06 +0200

    now it works

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
-#Check if a wordpress folder exists otherwise create it by downloading wordpress file
+#Download wordpress
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+rm -rf latest.tar.gz
 
-if [ -d "/var/www/html/wordpress" ]
-then
-	rm -rf www.conf
-	echo "wordpress already downloaded"
-else
-	#Download wordpress
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm -rf latest.tar.gz
+#Update configuration file
+rm -rf /etc/php/7.3/fpm/pool.d/www.conf
+mv ./www.conf /etc/php/7.3/fpm/pool.d/
 
-	#Update configuration file
-	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ./www.conf /etc/php/7.3/fpm/pool.d/
-	chown -R www-data: /etc/php/7.3/fpm/pool.d/www.conf
+#Inport env variables in the config file
+cd /var/www/html/wordpress
+sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+mv wp-config-sample.php wp-config.php
 
-	sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wordpress/wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wordpress/wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" /var/www/html/wordpress/wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config-sample.php
-	cd /var/www/html/wordpress
-	mv wp-config-sample.php wp-config.php
-fi
+exec "$@"
